aa71ffefe89b9a71f349d106d0c786fd0ffa1ac4
Bajinder Singh <bajindersingh@192.168.1.104>
2023-09-02 19:36:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(cors());
+
+// set the view engine to ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+// about page
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(8080);
+console.log("Server is listening on port 8080");
+module.exports = app;
